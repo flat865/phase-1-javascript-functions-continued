5677941833b3e1d52a9a1c12736d4c92242cd52e
Alfonso Lopez Cruz <alfonso2081lopez@gmail.com>
2022-01-20 18:54:27 -0500

    commit tree c7ecd8c76b62c58c59bcdcdc9948e6d3d0bc85d0

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`; 
+}
+function mondayWork(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`
+}
+function wrapAdjective(adjective = '*'){
+ const inside = function(activity = "special"){
+   return `You are ${adjective}${activity}${adjective}!`;
+}
+return inside
+}
+
